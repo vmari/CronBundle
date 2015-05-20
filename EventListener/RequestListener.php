8c735e8d42abec1bb78957bd16d64a231a431109
Valentin Mari <valentinmari@hotmail.com>
2015-05-20 20:12:18 -0300

    Add EventListener for a Request and DependencyInjection

---
+++ EventListener/RequestListener.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Crontab\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpFoundation\Response;
+
+class RequestListener{
+   public function onKernelRequest(GetResponseEvent $event)
+   {
+       $request = $event->getRequest();
+       $event->setResponse(new Response("snif"));
+   }
+}
