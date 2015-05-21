50ca0b72c3b884db2a9d0e379c6d9f12dc425b07
Valentin Mari <valentinmari@hotmail.com>
2015-05-20 23:27:13 -0300

    Inject Entity Manager

--- EventListener/RequestListener.php
+++ EventListener/RequestListener.php
@@ -4,11 +4,19 @@ namespace Crontab\EventListener;
 
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManager;
 
 class RequestListener{
-   public function onKernelRequest(GetResponseEvent $event)
-   {
-       $request = $event->getRequest();
-       //Cron.
-   }
+    protected $em;
+
+    public function __construct(EntityManager $em){
+        $this->em = $em;
+    }
+    
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        $crons = $this->em->getRepository('CrontabBundle:Cron')->findAll();
+        //$event->setResponse(new Response('<pre>'.print_r($crons,1)));
+    }
 }
